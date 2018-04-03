f72abade3bb237ab3bbe1334eef6401c46abf904
zouhong1991 <2393710200@qq.com>
2018-04-03 14:49:03 +0800

    word
    项目首次提交

---
+++ src/test/test.java
@@ -0,0 +1,16 @@
+package test;
+
+import com.shu.service.Function;
+import com.shu.service.FunctionService;
+
+public class test {
+	
+	public  static void main(String[] args){
+		Function fu =new FunctionService().getFunctionPort();
+		String str=fu.transWords("Let's Get Heck Out Of Here!");
+		//最后str就是在Trans项目下的transWords方法处理后的字符串咯!
+		System.out.println(str);
+	}
+
+
+}
